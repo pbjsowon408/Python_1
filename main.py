3588b09cea98d96bde619c649bf22f3447f24438
ByoungJun Park <33891528+pbjsowon408@users.noreply.github.com>
2020-05-09 21:28:51 +0900

    Add main.py
    get all the job info lists of Indeed.com Japan.

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+from indeed import extract_indeed_pages, extract_indeed_jobs
+
+
+last_indeed_page = extract_indeed_pages()
+
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
+
+print(indeed_jobs)
+
+
+# In[ ]:
+
+
+
+
