97797e1814462f02c124a17623a2ad365b6208db
ByoungJun Park <33891528+pbjsowon408@users.noreply.github.com>
2020-05-11 02:05:29 +0900

    Update main.py
    get job lists in Stack overflow website.
    
    extract indeed, stackoverflow lists to CSV

--- main.py
+++ main.py
@@ -5,10 +5,15 @@
 
 
 from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
+from save import save_to_file
 
+so_jobs = get_so_jobs()
 indeed_jobs = get_indeed_jobs()
+jobs = so_jobs + indeed_jobs
+save_to_file(jobs)
 
-print(indeed_jobs)
+##CSV = Comma Separated Values
 
 
 # In[ ]:
