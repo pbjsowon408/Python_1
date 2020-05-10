9b4279e2686b4f91ad6cd4e73baac468aadb02a9
ByoungJun Park <33891528+pbjsowon408@users.noreply.github.com>
2020-05-11 02:10:23 +0900

    Add so.py
    Add StackOverflow.py to get all the lists of job info

---
+++ so.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"https://stackoverflow.com/jobs?q=python"
+
+def get_last_pages():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text, "html.parser")
+  pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
+  last_page = pages[-2].get_text(strip=True)
+  return int(last_page)
+
+
+def extract_job(html):
+  title = html.find("div",{"class":"grid--cell fl1"}).find("a")["title"]
+  company, location = html.find("h3",{"class":"fc-black-700 fs-body1 mb4"}).find_all("span",recursive=False) 
+  # "Unpacking" very comfortable to make to var together 
+  # "recursive" only runs first seleced levels
+  company = company.get_text(strip=True).strip("•").strip("\r")
+  location = location.get_text(strip=True).strip("•").strip("\r")
+  job_id = html['data-jobid']
+
+  return {'title':title,'company':company,'location':location,"apply_link":f"https://stackoverflow.com/jobs/{job_id}"}
+
+def extract_jobs(last_page):
+    
+  jobs = []
+  for page in range(last_page):
+    print(f"Scrapping StackOverFlow with Page: {page}")
+    result = requests.get(f"{URL}&pg={page+1}")
+    soup = BeautifulSoup(result.text,"html.parser")
+    results = soup.find_all("div",{"class":"-job"})
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
+  return jobs
+
+def get_jobs():
+  last_page = get_last_pages()
+  jobs = extract_jobs(last_page)
+  return jobs
